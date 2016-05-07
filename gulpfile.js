1766544c7ad491c4a4fcf305082a919d66553c3c
Joseph M. Lloyd <joe.lloyd.22.24@gmail.com>
2016-05-07 11:14:06 +0200

    server generated with gulp now

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,33 @@
 'use strict';
 
 var gulp = require('gulp');
-var browserSync = require('browser-sync');
 var nodemon = require('gulp-nodemon');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
+var babel = require('gulp-babel');
 
-gulp.task('start', ['browser-sync']);
+// var browserify = require('browserify');
+// var babelify = require('babelify');
+// var source = require('vinyl-source-stream');
 
-gulp.task('browser-sync', ['nodemon'], function() {
-
-	browserSync.init(null, {
-		proxy: "localhost:3000",
-        browser: "google chrome",
-        port: 7000
-	});
-
-});
-
-gulp.task('nodemon', function (cb) {
-    return nodemon({
-      script: 'server.js'
-    }).once('start', cb);
+gulp.task('start', function (cb) {
+	return nodemon({
+		script: 'index.js'
+	}).once('start', cb);
 });
 
 // gulp.task('buildServer', function () {
-//     return browserify({entries: './server/index.js', extensions: ['.js'], debug: true})
-//         .transform('babelify', {presets: ['es2015', 'react']})
-//         .bundle()
-//         .pipe(source('server.js'))
-//         .pipe(gulp.dest('./'));
+// 	return browserify({entries: './server/index.js', extensions: ['.js'], debug: true})
+// 	.transform('babelify', {presets: ['es2015', 'react']})
+// 	.bundle()
+// 	.pipe(source('server.js'))
+// 	.pipe(gulp.dest('./'));
 // });
 
+
+
+gulp.task('buildServer', () =>
+	gulp.src('./server/index.js')
+	.pipe(babel({
+		presets: ['es2015', 'react']
+	}))
+	.pipe(gulp.dest('.'))
+);

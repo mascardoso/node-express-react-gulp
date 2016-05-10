69d59b4761efc341067d2926c7ffec902da67521
joelloyd <joe.lloyd.22.24@gmail.com>
2016-05-10 11:19:54 +0200

    basic refactor done

---
+++ gulp-tasks/build-resources.js
@@ -0,0 +1,52 @@
+var babel = require('gulp-babel');
+var rename = require("gulp-rename");
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var browserify = require("browserify");
+var gutil = require("gulp-util");
+var source = require("vinyl-source-stream");
+var babelify = require("babelify");
+
+module.exports = {
+
+	server: function(gulp, CLIENT_COMP_DIR, SERVER_GEN_DIR) {
+
+		gulp.src(CLIENT_COMP_DIR + '/**/*.js')
+		.pipe(babel({
+			presets: ['es2015', 'react']
+		}))
+		.pipe(gulp.dest(SERVER_GEN_DIR))
+
+	},
+
+	client: function(gulp, CLIENT_DIR, PUBLIC_DIR) {
+
+		browserify({
+			debug: true,
+	    	entries: [CLIENT_DIR + '/index.js']
+		})
+	    .transform(babelify.configure({
+	        sourceMapRelative: CLIENT_DIR,
+	        presets: ["es2015", "react"]
+	    }))
+		.bundle()
+		.on('error', function(e){
+			gutil.log(e);
+		})
+		.pipe(source('bundle.js'))
+		.pipe(rename('bundle.min.js'))
+		.pipe(gulp.dest(PUBLIC_DIR));
+
+	},
+
+	sass: function(gulp, CLIENT_COMP_DIR, PUBLIC_DIR) {
+
+		return gulp.src(CLIENT_COMP_DIR + '/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(rename('bundle.min.css'))
+		.pipe(gulp.dest(PUBLIC_DIR));
+		
+	}
+
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-var babel = require('gulp-babel');
-var rename = require("gulp-rename");
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var browserify = require("browserify");
-var gutil = require("gulp-util");
-var source = require("vinyl-source-stream");
-var babelify = require("babelify");
 var path = require("path");
-var gutil = require('gulp-util');
 
 const CLIENT_DIR = path.resolve(__dirname, 'client');
 const CLIENT_COMP_DIR = path.resolve(__dirname, 'client/components');
@@ -20,60 +9,26 @@ const SERVER_DIR = path.resolve(__dirname, 'server');
 const SERVER_GEN_DIR = path.resolve(__dirname, 'server/generated');
 const PUBLIC_DIR = path.resolve(__dirname, 'public');
 
-//Runs the server
+
+// Runs the server
 gulp.task('start', function (cb) {
-	return nodemon({
-		script: 'server/server.js'
-	}).once('start', cb);
+	require('./gulp-tasks/run-server.js')(gulp, cb);
 });
-
-// Builds the server
-// gulp.task('buildServer', () =>
-// 	gulp.src([(SERVER_DIR + '**/**/*.js'), ('!' + SERVER_DIR + '/generated/**/**/.js')])
-// 	.pipe(babel({
-// 		presets: ['es2015', 'react']
-// 	}))
-// 	// .pipe(rename('server.js'))
-// 	.pipe(gulp.dest('./build'))
-// );
-
 
 // builds the components serverside
-gulp.task('buildComponentsServer', () =>
-	gulp.src(CLIENT_COMP_DIR + '/**/*.js')
-	.pipe(babel({
-		presets: ['es2015', 'react']
-	}))
-	.pipe(gulp.dest(SERVER_GEN_DIR))
-);
+gulp.task('buildComponentsServer', function() {
+	require('./gulp-tasks/build-resources.js').server(gulp, CLIENT_COMP_DIR, SERVER_GEN_DIR);
+});
 
-//builds the client minified files
+// builds the client minified files
 gulp.task('buildComponentsClient', function(){
-	browserify({
-		debug: true,
-    	entries: [CLIENT_DIR + '/index.js']
-	})
-    .transform(babelify.configure({
-        sourceMapRelative: CLIENT_DIR,
-        presets: ["es2015", "react"]
-    }))
-	.bundle()
-	.on('error', function(e){
-		gutil.log(e);
-	})
-	.pipe(source('bundle.js'))
-	.pipe(rename('bundle.min.js'))
-	.pipe(gulp.dest(PUBLIC_DIR));
+	require('./gulp-tasks/build-resources.js').client(gulp, CLIENT_DIR, SERVER_GEN_DIR);
 });
 
-
-//builds sass files
+// builds sass files
 gulp.task('sass', function () {
-	return gulp.src(CLIENT_COMP_DIR + '/**/*.scss')
-	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-	.pipe(rename('bundle.min.css'))
-	.pipe(gulp.dest(PUBLIC_DIR));
+	require('./gulp-tasks/build-resources.js').sass(gulp, CLIENT_COMP_DIR, PUBLIC_DIR);
 });
 
-//build
+// build
 gulp.task('build', ['buildComponentsServer', 'buildComponentsClient', 'sass']);

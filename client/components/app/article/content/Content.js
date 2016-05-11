1396bb538fb5f2cd76f040e04dfc70be0d568482
Alessandro Rabitti <alessandro.rabitti@thesecretlab.nl>
2016-05-11 09:17:37 +0200

    cleaned code

--- client/components/app/article/content/Content.js
+++ client/components/app/article/content/Content.js
@@ -8,7 +8,7 @@ class Content extends React.Component {
 
 	render() {
 		return (
-			<div><p>This is pure content CIAO {this.props.test}</p></div>
+			<div><p>This is pure content {this.props.test}</p></div>
 		);
 	}
 }
--- server/routes/article.js
+++ server/routes/article.js
@@ -2,6 +2,7 @@ var React = require('react');
 var express = require('express');
 var Router = express.Router();
 var ReactDOMServer = require('react-dom/server');
+
 var Element = React.createElement(require('../generated/app/App.js').default, null);
 
 //console.log(require('../generated/app/index.js').default);
